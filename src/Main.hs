cc2c34b8e665aaad3b1fc32bb94656fafc51958e
damianfral <huevofritopamojarpan@gmail.com>
2015-11-02 12:43:49 +0100

    Remove unneeded imports.

--- src/Main.hs
+++ src/Main.hs
@@ -8,10 +8,6 @@ import           BasicPrelude              hiding (isPrefixOf, lookup, try,
                                             (<|>))
 import           Control.Monad.Trans.State
 import           Data.Aeson
-import qualified Data.ByteString.Lazy      as BS
-import           Data.HashMap.Lazy         hiding (filter, (!))
-import           Data.Text                 (index, isPrefixOf, pack)
-import           Data.Time.Calendar        (showGregorian)
 import           GHCanIUse
 import           Lucid
 import           Turtle.Prelude
