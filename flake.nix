1204c399a181a6e4a36824c6597d1a643418a44b
damianfral <huevofritopamojarpan@gmail.com>
2023-06-09 22:06:43 +0000

    Refactor flake.

--- flake.nix
+++ flake.nix
@@ -2,55 +2,139 @@
   description = "angelagesteira.com website";
 
   inputs = {
-    nixpkgs = { url = "github:NixOS/nixpkgs/release-21.11"; };
+    nixpkgs = { url = "github:NixOS/nixpkgs"; };
+    nixpkgs-1803 = { url = "github:NixOS/nixpkgs/release-18.03"; flake = false; };
     nixpkgs-2003 = { url = "github:NixOS/nixpkgs/release-20.03"; };
-    nixops-plugged = { url = "github:lukebfox/nixops-plugged"; };
+    nixpkgs-2211 = { url = "github:NixOS/nixpkgs/release-22.11"; };
+    nixpkgs-2305 = { url = "github:NixOS/nixpkgs/release-23.05"; };
     flake-utils = { url = "github:numtide/flake-utils"; };
+    nix-filter.url = "github:numtide/nix-filter";
+    # needed for systest
+    safe-coloured-text.url = "github:NorfairKing/safe-coloured-text";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , ...
-    }@inputs:
+  outputs = { self, flake-utils, ... }@inputs:
 
     let
-      pkgsFor = system: import nixpkgs {
-        inherit system;
-        overlays = [ ];
+      pkgsFor = nixpkgs: system:
+        import nixpkgs {
+          inherit system;
+          overlays = [
+            self.overlays.default
+            inputs.nix-filter.overlays.default
+            inputs.safe-coloured-text.overlays.${system}
+          ];
+        };
+      src = inputs.nix-filter.lib {
+        root = ./.;
+        include = [
+          "src"
+          "assets"
+          "package.yaml"
+        ];
       };
-
     in
+    {
+      overlays.default = final: prev: with final.haskell.lib; {
+        haskellPackages = prev.haskellPackages.override
+          (old: {
+            overrides = final.lib.composeExtensions
+              (old.overrides or (_: _: { }))
+              (self: super: {
+                autodocodec-yaml = unmarkBroken (doCheck super.autodocodec-yaml);
+                sydtest = unmarkBroken (dontCheck super.sydtest);
+                ghcaniuse = dontCheck (self.callCabal2nix "ghcaniuse" src { });
+              });
+          });
+      };
+    }
+    //
     flake-utils.lib.eachDefaultSystem
       (system:
 
-      let
-        pkgs = pkgsFor system;
-        haskellPackages = pkgs.haskellPackages.extend
-          (pkgs.haskell.lib.compose.packageSourceOverrides {
-            ghcaniuse = ./.;
-          });
-      in
-      rec {
-        packages = {
-          ghcaniuse = haskellPackages.ghcaniuse;
-        };
+        let
+          pkgs = pkgsFor inputs.nixpkgs system;
 
-        defaultPackage = packages.ghcaniuse;
-        devShells.default =
-          haskellPackages.shellFor
-            {
-              packages = p: [ packages.ghcaniuse ];
-              buildInputs = with pkgs; [ haskell-language-server cabal-install ghcid haskellPackages.implicit-hie ];
+          pkgs1803 = pkgsFor inputs.nixpkgs-1803 system;
+          pkgs2003 = pkgsFor inputs.nixpkgs-2003 system;
+          pkgs2211 = pkgsFor inputs.nixpkgs-2211 system;
+          pkgs2305 = pkgsFor inputs.nixpkgs-2305 system;
+          mkDerivationLanguageExtensions = name: ghc:
+            pkgs.stdenv.mkDerivation {
+              name = "language-extensions-for-${name}";
+              phases = [ "buildPhase" "installPhase" ];
+              buildInputs = [ ghc ];
+              buildPhase = "ghc --supported-languages > ${name}-languages.txt";
+              installPhase = "mkdir -p $out; cp ${name}-languages.txt $out";
             };
-        deploy-shell = pkgs.mkShell {
-          buildInputs = [ inputs.nixops-plugged.defaultPackage.${system} ];
-          shellHook = ''
-            export NIX_PATH="nixpkgs=${nixpkgs}"
-            export NIXOPS_STATE="./state.nixops"
-          '';
-        };
-      }
-      );
+          ghcs = {
+            "ghc-7.0.4" = pkgs1803.haskell.compiler.ghc704Binary;
+            "ghc-7.4.2" = pkgs1803.haskell.compiler.ghc742Binary;
+            "ghc-7.6.3" = pkgs1803.haskell.compiler.ghc763;
+            "ghc-7.8.4" = pkgs1803.haskell.compiler.ghc784Binary;
+            "ghc-7.10.3" = pkgs1803.haskell.compiler.ghc7103Binary;
+            "ghc-8.0.2" = pkgs1803.haskell.compiler.ghc802;
+            "ghc-8.2.2" = pkgs2003.haskell.compiler.ghc822Binary;
+            "ghc-8.4.4" = pkgs2003.haskell.compiler.ghc844;
+            "ghc-8.6.5" = pkgs2211.haskell.compiler.ghc865Binary;
+            "ghc-8.8.4" = pkgs2305.haskell.compiler.ghc884;
+            "ghc-8.10.7" = pkgs2305.haskell.compiler.ghc8107;
+            "ghc-9.0.2" = pkgs2305.haskell.compiler.ghc902;
+            "ghc-9.2.6" = pkgs2305.haskell.compiler.ghc926;
+            "ghc-9.4.5" = pkgs2305.haskell.compiler.ghc945;
+            "ghc-9.6.2" = pkgs2305.haskell.compiler.ghc962;
+          };
+        in
+        rec {
+          packages = {
+            ghc-language-extensions = pkgs.symlinkJoin {
+              name = "ghc-language-extensions";
+              paths = builtins.attrValues (
+                (builtins.mapAttrs mkDerivationLanguageExtensions)
+                  ghcs
+              );
+            };
+            ghcaniuse = pkgs.haskellPackages.ghcaniuse;
+            ghcaniuse-web = pkgs.stdenv.mkDerivation {
+              name = "ghcaniuse-web";
+              phases = [ "buildPhase" "installPhase" ];
+              buildPhase = apps.ghcaniuse-gen.program;
+              installPhase = ''
+                mkdir -p $out
+                cp index.html $out
+              '';
+            };
+            default = packages.ghcaniuse-web;
+          };
+          apps = {
+            ghcaniuse = flake-utils.lib.mkApp {
+              drv = pkgs.haskell.lib.justStaticExecutables (
+                packages.ghcaniuse.overrideAttrs (oldAttrs: {
+                  configureFlags = oldAttrs.configureFlags ++ [ "--ghc-option=-O2" ];
+                })
+              );
+            };
+            ghcaniuse-gen = flake-utils.lib.mkApp {
+              drv = pkgs.writeShellScriptBin "ghcaniuse-gen" ''
+                ${apps.ghcaniuse.program} \
+                  --directory ${packages.ghc-language-extensions} $@
+              '';
+            };
+            default = apps.ghcaniuse-gen;
+          };
+
+          devShells = {
+            default = pkgs.haskellPackages.shellFor {
+              packages = p: [ (pkgs.haskell.lib.doCheck packages.ghcaniuse) ];
+              buildInputs = with pkgs; [
+                nixpkgs-fmt
+                cabal-install
+                hpack
+                ghcid
+                haskellPackages.implicit-hie
+                haskell-language-server
+              ];
+            };
+          };
+        });
 }
