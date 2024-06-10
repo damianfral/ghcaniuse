0e1e22ca0728f18e218a949d2dc6f463e8d65282
damianfral <huevofritopamojarpan@gmail.com>
2024-06-10 00:49:47 +0000

    Update some ghc versions.

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "angelagesteira.com website";
 
   inputs = {
-    nixpkgs = { url = "github:NixOS/nixpkgs"; };
     nixpkgs-1803 = { url = "github:NixOS/nixpkgs/release-18.03"; flake = false; };
     nixpkgs-2003 = { url = "github:NixOS/nixpkgs/release-20.03"; };
     nixpkgs-2305 = { url = "github:NixOS/nixpkgs/release-23.05"; };
+    nixpkgs-2405 = { url = "github:NixOS/nixpkgs/release-24.05"; };
     flake-utils = { url = "github:numtide/flake-utils"; };
     nix-filter.url = "github:numtide/nix-filter";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
-    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
+    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs-2405";
   };
 
   outputs = { self, flake-utils, ... }@inputs:
@@ -52,11 +52,11 @@
       (system:
 
         let
-          pkgs = pkgsFor inputs.nixpkgs system;
-
+          pkgs = pkgs2405;
           pkgs1803 = pkgsFor inputs.nixpkgs-1803 system;
           pkgs2003 = pkgsFor inputs.nixpkgs-2003 system;
           pkgs2305 = pkgsFor inputs.nixpkgs-2305 system;
+          pkgs2405 = pkgsFor inputs.nixpkgs-2405 system;
           mkDerivationLanguageExtensions = name: ghc:
             pkgs.stdenv.mkDerivation {
               name = "language-extensions-for-${name}";
@@ -78,10 +78,10 @@
             "ghc-8.8.4" = pkgs2305.haskell.compiler.ghc884;
             "ghc-8.10.7" = pkgs2305.haskell.compiler.ghc8107;
             "ghc-9.0.2" = pkgs2305.haskell.compiler.ghc902;
-            "ghc-9.2.6" = pkgs2305.haskell.compiler.ghc926;
-            "ghc-9.4.5" = pkgs2305.haskell.compiler.ghc945;
-            "ghc-9.6.2" = pkgs2305.haskell.compiler.ghc962;
-            "ghc-9.8.1" = pkgs.haskell.compiler.ghc981;
+            "ghc-9.2.8" = pkgs2405.haskell.compiler.ghc928;
+            "ghc-9.4.6" = pkgs2405.haskell.compiler.ghc946;
+            "ghc-9.6.5" = pkgs2405.haskell.compiler.ghc965;
+            "ghc-9.8.2" = pkgs2405.haskell.compiler.ghc982;
           };
           precommitCheck = inputs.pre-commit-hooks.lib.${system}.run {
             src = ./.;
@@ -144,6 +144,7 @@
                 hpack
                 ghcid
                 haskellPackages.implicit-hie
+                haskellPackages.calligraphy
                 haskell-language-server
               ];
               inherit (precommitCheck) shellHook;
